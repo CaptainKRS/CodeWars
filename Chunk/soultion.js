c7c7ee54604ebdd43be26fdde4e500c50a43fe31
Kirk Sullivan <kirk_135@hotmail.com>
2018-09-12 16:24:57 -0400

    Soultion to the Chunk Array problem

---
+++ Chunk/soultion.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+const chunk = (array, size) => {
+ let chunkArr = []
+ let index = 0;
+ while(index < array.length){
+   chunkArr.push(array.slice(index, index+ size))
+   index+= size
+ }
+ return chunkArr
+}
+
+
+// function chunk(array, size) {
+//   let chunkArr = [];
+//   for(var element of array) {
+//     const last = chunkArr[chunkArr.length-1];
+//     if(!last || last.length === size){
+//       chunkArr.push([element])
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//   return chunkArr
+// }
